6ea8b3fcd61883a2b14f46914678284bc324df28
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-02-08 00:50:22 +0100

    feat/Initial Spring structure and dependencies

---
+++ src/main/java/com/predu/evertask/EvertaskApplication.java
@@ -0,0 +1,13 @@
+package com.predu.evertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvertaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/predu/evertask/EvertaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predu.evertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvertaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

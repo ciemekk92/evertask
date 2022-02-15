1ac168a4f883ac85e638890eb523fc9e7084db1d
p-reducha <przemyslaw.reducha@spirent.com>
2022-02-15 16:37:21 +0100

    Feature: Add docker-compose dev environment with PostgreSQL and basic front-end skeleton

---
+++ src/main/java/com/predu/evertask/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.predu.evertask.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> sayHello() {
+        return ResponseEntity.ok("HELLO!");
+    }
+}

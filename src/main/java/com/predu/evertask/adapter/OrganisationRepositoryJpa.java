fd23fa2ca28234c32a624d41d6da2b12c75704c0
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-04-18 17:01:15 +0200

    WIP: Adjust OrganisationRepository implementation

--- src/main/java/com/predu/evertask/adapter/OrganisationRepositoryJpa.java
+++ src/main/java/com/predu/evertask/adapter/OrganisationRepositoryJpa.java
@@ -3,23 +3,19 @@ package com.predu.evertask.adapter;
 import com.predu.evertask.domain.model.Organisation;
 import com.predu.evertask.exception.NotFoundException;
 import com.predu.evertask.repository.OrganisationRepository;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrganisationRepositoryJpa extends OrganisationRepository, JpaRepository<Organisation, UUID> {
 
-    default Organisation getById(UUID id) {
-        Optional<Organisation> optionalOrganisation = findById(id);
-
-        if (optionalOrganisation.isEmpty()) {
-            throw new NotFoundException("Organisation not found");
-        }
-
-        return optionalOrganisation.get();
+    @NonNull
+    default Organisation getById(@NonNull UUID id) {
+        return findById(id)
+                .orElseThrow(() -> new NotFoundException(Organisation.class, id));
     }
 }
 

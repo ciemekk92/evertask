71931030382580416a0fc20d1ca8854161e88412
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-08-08 08:41:24 +0200

    WIP

---
+++ frontend/src/Modules/Statistics/Statistics.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const Statistics = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return <div></div>;
+};

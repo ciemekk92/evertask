37bf17252f2f73002376a91044e8fe413712a0ba
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-11-19 10:25:46 +0100

    Fix: Fix issue description padding

--- frontend/src/Modules/IssuePage/components/IssueDescriptionSection/IssueDescriptionSection.styled.ts
+++ frontend/src/Modules/IssuePage/components/IssueDescriptionSection/IssueDescriptionSection.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledDescriptionContainer = styled.div`
   font-size: 1.6rem;
-  padding: 1rem;
+  padding: 1rem 1.5rem;
   white-space: pre-wrap;
   max-height: 40rem;
   overflow: auto;
